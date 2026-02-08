fc20bd6e2a744dd027e84425c2df2d8d7fb35847
Pavel Shvedko <shved@mail.ru>
2026-02-08 19:46:49 +0300

    Update geo.sql

--- sql/schema/geo.sql
+++ sql/schema/geo.sql
@@ -6,4 +6,3 @@ CREATE TABLE IF NOT EXISTS points
     point GEOMETRY(Point, 4326) NOT NULL -- 4326 это WGS84 (стандарт для GPS)
 );
 CREATE INDEX IF NOT EXISTS points_point_idx ON points USING GIST (point);
-CLUSTER points USING points_point_idx;
